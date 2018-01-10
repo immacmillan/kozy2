2d0275ab25fbec6a9f76f6c7a08256441a88752a
immacmillan <30418708+immacmillan@users.noreply.github.com>
2018-01-10 13:52:42 -0500

    fixed angular form references

--- root.js
+++ root.js
@@ -1,25 +1,11 @@
 var app = angular.module('root', []);
-
-app.controller('formCntrl', ["$scope", "$http", function($scope, $http) {
-    $scope.master = {
-      firstName: 'First Name',
-      lastName: 'Last Name',
-      email: 'Email'
-    };
+app.controller('formCntrl', function($scope) {
+    $scope.firstnameone = '';
+    $scope.lastnameone = '';
+    $scope.email = {
+        text: ''
+      };
     $scope.submitone = function() {
-      var Indata = {params:user.firstName, params}
-         $http({
-         url: "time.php",
-         method: "POST",
-         params: Indata
-         })
-        $http.post("customers_mysql.php", )
-          .then(function (response) {$scope.names = response.data.records;});
-            $scope.user = angular.copy($scope.master);
-            alert("The get function worked")
-    };
-
-  };
-
-
-}]);
+		alert('Thanks for submitting')
+        };
+    });
